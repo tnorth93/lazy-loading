cbee6bd918ab51d219b39fc39fc4ad1d3fbaa58b
Tom North <tnorth93@gmail.com>
2020-08-17 15:38:17 -0700

    added lazy routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
-import Posts from './containers/Posts';
 import User from './containers/User';
 import Welcome from './containers/Welcome';
+
+const Posts = React.lazy(() => import('./containers/Posts'));
 
 class App extends Component {
   render() {
